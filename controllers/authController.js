fedf1f5218932f9a0a9b028a64e26974185fd883
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-08-27 23:10:57 +0100

    add catch block

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,44 +2,53 @@ const { hash } = require("bcrypt");
 const User = require("../models/user.js");
 
 exports.register = async function (req, res, next) {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    // use schema to validate
-    if (!name) {
-        res.status(422).json({ message: "A name is required." });
-        return;
-    }
+        // use schema to validate
+        if (!name) {
+            res.status(422).json({ message: "A name is required.", error: {} });
+            return;
+        }
 
-    if (!email || !password) {
-        res.status(422).json({ message: "Email and password are required!" });
-        return;
-    }
+        if (!email || !password) {
+            res.status(422).json({
+                message: "Email and password are required!",
+                error: {},
+            });
+            return;
+        }
 
-    let existingUser = await User.findOne({ email: email });
-    if (existingUser) {
-        res.status(400).send({
-            message: "An account exists with this email address.",
+        let existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            res.status(400).send({
+                message: "An account exists with this email address.",
+                error: {},
+            });
+            return;
+        }
+
+        const passwordHash = await hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            passwordHash,
         });
+
+        const error = await user.validate();
+        if (error) {
+            console.error(error);
+            res.status(422).send({ message: error, error: {} });
+            return;
+        }
+
+        await user.save();
+        console.log("User created.");
+        res.send(user.dto);
         return;
+    } catch (error) {
+        next(error);
     }
-
-    const passwordHash = await hash(password, 10);
-    const user = new User({
-        name,
-        email,
-        passwordHash,
-    });
-
-    const error = await user.validate();
-    if (error) {
-        console.error(error);
-        res.status(422).send({ message: error });
-        return;
-    }
-
-    console.log("Validation successful.");
-    await user.save();
-    console.log("User created.");
-    res.send(user.dto);
-    return;
 };
+
+// export
