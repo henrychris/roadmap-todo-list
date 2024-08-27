966b60eb5a4a2a06bb6a31feddac871dc45ee9ca
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-08-27 22:51:19 +0100

    add user schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        passwordHash: { type: String, required: true, unique: true },
+    },
+    { timestamps: true } // auto add createdAt and updatedAt
+);
+
+UserSchema.virtual("dto").get(function () {
+    return {
+        id: this._id,
+        name: this.name,
+        email: this.email,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+    };
+});
+
+module.exports = mongoose.model("User", UserSchema);
