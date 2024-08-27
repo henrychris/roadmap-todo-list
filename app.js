de46b6cca4a808053f14f724e0c4a7ae724eef4d
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-08-27 22:17:33 +0100

    organise stuff

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 require("dotenv").config();
-const createError = require("http-errors");
 const express = require("express");
 const logger = require("morgan");
 const db = require("./db");
+const errorHandler = require("./middleware/errorHandler.js");
 const PORT = process.env.PORT;
-
-db.connect().catch((err) => console.log(err));
 
 const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use(function (req, res, next) {
-    next(createError(404));
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+// Error handler middleware
+app.use(errorHandler);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
-
-app.listen(PORT, function () {
-    console.log(`Example app listening on http://localhost:${PORT}!`);
-});
+db.connect()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to database:", err);
+        process.exit(1);
+    });
