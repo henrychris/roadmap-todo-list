9deefff0df1ab2b0a07a10d83282997c6fda7457
henrychris <64085210+henrychris@users.noreply.github.com>
2024-09-24 20:39:00 +0100

    inline createToken methods

--- utils/token.js
+++ utils/token.js
@@ -2,22 +2,21 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET, REFRESH_TOKEN_SECRET } = require("../config.js");
 
 exports.createToken = function (userId, email) {
-    const token = jwt.sign({ id: userId, email: email }, JWT_SECRET, {
+    return jwt.sign({ id: userId, email: email }, JWT_SECRET, {
         expiresIn: "1h",
     });
-    return token;
 };
 
 exports.createRefreshToken = function (userId, email) {
-    const token = jwt.sign({ id: userId, email: email }, REFRESH_TOKEN_SECRET, {
+    return jwt.sign({ id: userId, email: email }, REFRESH_TOKEN_SECRET, {
         expiresIn: "7d",
     });
-    return token;
 };
 
 exports.validateRefreshToken = function (refreshToken) {
     try {
         const decoded = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+
         console.log(`Refresh token valid for user id: ${decoded.id}`);
         return { userId: decoded.id, email: decoded.email };
     } catch (error) {
