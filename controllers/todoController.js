ea7a38890eb0ebc5644c7226d87e18b857048f1f
henrychris <64085210+henrychris@users.noreply.github.com>
2024-09-24 12:28:28 +0100

    implement create todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,7 +3,27 @@ const Todo = require("../models/todo.js");
 
 exports.create = async function (req, res, next) {
     try {
-        res.status(200).send("todo create");
+        const { title, description } = req.body;
+
+        const user = await User.findOne({ email: req.email });
+        if (!user) {
+            return res.status(401).send({
+                message: "user not found.",
+                error: {},
+            });
+        }
+
+        const todo = new Todo({
+            title: title,
+            description: description,
+            userId: req.userId,
+        });
+
+        await todo.validate();
+        await todo.save();
+
+        console.log("Todo created.");
+        res.status(200).send(todo.dto);
     } catch (error) {
         next(error);
     }
