f7a1d54ac1bc324829f7bf5a88719a90bcbab3b5
henrychris <64085210+henrychris@users.noreply.github.com>
2024-09-30 11:59:28 +0100

    delete all js files

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { DB_URL } = require("./config.js");
-
-exports.connect = async function main() {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(DB_URL);
-    console.log(`Connected to mongoose on URL: ${DB_URL}`);
-};
